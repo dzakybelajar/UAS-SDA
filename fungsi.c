221f3be7eb0920b1c7bec9c2d85183b983e0dc8b
dzakybelajar <dzakysyraj@mhs.usk.ac.id>
2026-06-03 21:57:48 +0700

    hgds

--- fungsi.c
+++ fungsi.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+#include"header.h"
 
 typedef struct Tugas
 {
@@ -13,41 +14,3 @@ typedef struct Tugas
 }Tugas;
 
 Tugas tugas[20];
-
-void menu_utama(){
-    int pilihan;
-    do
-    {
-        pilihan=0;
-        printf("====menu====\n");
-        printf("1.Tambah tugas\n");
-        printf("2.Cari tugas\n");
-        printf("3.Hapus tugas\n");
-        printf("4.Lihat tugas paling mendesak\n");
-        printf("5.Urutkan tugas berdasarkan bobot nilai\n");
-        printf("pilih menu:");
-        scanf("%d",&pilihan);
-        while(getchar()!="\n");
-        switch (pilihan)
-        {
-        case 1:
-            tambah_tugas();
-            break;
-        case 2:
-            cari_tugas();
-            break;
-        case 3:
-            hapus_tugas();
-            break;
-        case 4:
-            lihat_tugas_paling_mendesak();
-            break;
-        case 5:
-            urutkan_tugas_berdasarkan_bobot_nilai();
-            break;
-        default:
-            break;
-        }
-    } while (pilihan!=0);
-    
-}
--- main.c
+++ main.c
@@ -1,8 +1,45 @@
 #include<stdio.h>
+#include"header.h"
 
+void menu_utama(){
+    int pilihan;
+    do
+    {
+        pilihan=0;
+        printf("====menu====\n");
+        printf("1.Tambah tugas\n");
+        printf("2.Cari tugas\n");
+        printf("3.Hapus tugas\n");
+        printf("4.Lihat tugas paling mendesak\n");
+        printf("5.Urutkan tugas berdasarkan bobot nilai\n");
+        printf("pilih menu:");
+        scanf("%d",&pilihan);
+        while(getchar()!="\n");
+        switch (pilihan)
+        {
+        case 1:
+            tambah_tugas();
+            break;
+        case 2:
+            cari_tugas();
+            break;
+        case 3:
+            hapus_tugas();
+            break;
+        case 4:
+            lihat_tugas_paling_mendesak();
+            break;
+        case 5:
+            urutkan_tugas_berdasarkan_bobot_nilai();
+            break;
+        default:
+            printf("input tidak valid!\n");
+            break;
+        }
+    } while (pilihan!=0);
+}
 
 int main(){
-
-    
+    menu_utama();
     return 0;
 }
